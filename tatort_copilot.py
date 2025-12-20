65b25533888af7f72a1295c6fcccec66239d0399
Nils-ChristianIseke <nilsmailiseke@gmail.com>
2025-12-20 23:07:40 +0100

    Ignoring somme ruff messages

--- tatort_copilot.py
+++ tatort_copilot.py
@@ -24,7 +24,7 @@ wort_zahlen = {
 }
 
 
-def parse_artikel(url: set) -> dict:
+def parse_artikel(url: set) -> dict:  # noqa: C901, PLR0912, PLR0914
     logger.info('Lade Artikel: %s', url)
     response = requests.get(url, timeout=100)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -89,7 +89,6 @@ def parse_artikel(url: set) -> dict:
         if p_tag:
             bewertung_text = p_tag.get_text(strip=True)
 
-            # Zahl extrahieren (z. B. "3 von 10 Punkten")
             match = re.search(r'\b(\d{1,2})\b', bewertung_text)
             if match:
                 bewertung = int(match.group(1))
